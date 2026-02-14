606abcab71e3f4146527b10f7c02545115b8fbc8
kevinkeyssx <keyss1700@gmail.com>
2026-02-14 00:44:47 -0300

    Footer added

---
+++ src/routes/components/Footer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import PowerButton from "$lib/components/buttons/PowerButton.svelte";
+</script>
+
+
+<footer class="py-5 px-4 border-t border-border">
+    <div class="max-w-7xl mx-auto text-center flex flex-col items-center">
+        <p class="text-sm text-muted-foreground">
+            Contenido basado en el manga y anime Hunter x Hunter de Yoshihiro Togashi.
+            Este es un proyecto de fans sin fines de lucro.
+        </p>
+
+        <p class="text-sm text-primary mb-2">
+            Desarrollado por:
+        </p>
+
+        <PowerButton />
+    </div>
+</footer>
