62dd4cbac7c1a1c9b2bcb0f2fe20bec39814fd78
kevinkeyssx <keyss1700@gmail.com>
2026-09-22 15:38:14 -0300

    Sounds optimized & md deleted

--- src/lib/utils/player.ts
+++ src/lib/utils/player.ts
@@ -1,7 +1,7 @@
 export const SOUND_FILES = {
-    SELECTED    : '/sounds/selected.wav',
-    CALCULATING : '/sounds/loading.wav',
-    RESULT      : '/sounds/celebrate.wav',
+    SELECTED    : '/sounds/selected.webm',
+    CALCULATING : '/sounds/loading.webm',
+    RESULT      : '/sounds/celebrate.webm',
     BACKGROUND  : '/sounds/background.mp3',
     INVOKE      : '/sounds/invoke/invoke-*.ogg',
 }
